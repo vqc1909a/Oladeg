890a7f3e1b176564f7c90ed963e641facf788c55
Nero Angel <neroangel1909@gmail.com>
2021-05-02 13:10:40 -0500

    Oladeg 1.8

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ require('dotenv').config();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+})
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
